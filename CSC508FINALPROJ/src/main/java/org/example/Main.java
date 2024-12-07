c23a13f22dfeff0a08cdd18158f9ac735a326c9d
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-12-06 18:21:28 -0800

    Update Main.java

--- CSC508FINALPROJ/src/main/java/org/example/Main.java
+++ CSC508FINALPROJ/src/main/java/org/example/Main.java
@@ -1,18 +1,13 @@
 package org.example;
 
 import javax.swing.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            logger.info("Starting the Cobot Control Application...");
             JFrame frame = new JFrame("Cobot Control");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(400, 300);
+            frame.setSize(800, 400);
             frame.add(new CobotControlPanel());
             frame.setVisible(true);
         });
